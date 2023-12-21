7a73cf2905ad8d479f65a21761659aa2cafaed60
Nathan <nathanfenwick@rocketmail.com>
2023-12-22 03:21:55 +0400

    Moved _generator_helper.py into overworld_generator.py.
    Extended new world page logic to allow for customisable noise generator parameters.

--- map_tooling/overworld_generator.py
+++ map_tooling/overworld_generator.py
@@ -1,14 +1,35 @@
-from map_tooling import _generator_helper
+import noise
+import numpy as np
+from PIL import Image
 
 
-def generate_overworld():
+def generate_overworld(file_name, scale, octaves, persistence, lacunarity):
     width, height = 1024, 1024
-    scale = 100.0  # Affects the "zoom" of the noise pattern
-    octaves = 6  # Affects the detail of the map
-    persistence = 0.5
-    lacunarity = 2.0
+    noise_map = generate_noise_map(width, height, scale, octaves, persistence, lacunarity)
+    normalized_map = normalize_map(noise_map)
+    create_image(normalized_map, file_name)
 
-    noise_map = _generator_helper.generate_noise_map(width, height, scale, octaves, persistence, lacunarity)
-    normalized_map = _generator_helper.normalize_map(noise_map)
-    _generator_helper.create_image(normalized_map, './saves/overworld_map.png')
 
+def generate_noise_map(width, height, scale, octaves, persistence, lacunarity):
+    world_map = np.zeros((height, width))
+    for y in range(height):
+        for x in range(width):
+            world_map[y][x] = noise.pnoise2(x / scale,
+                                            y / scale,
+                                            octaves=octaves,
+                                            persistence=persistence,
+                                            lacunarity=lacunarity)
+    return world_map
+
+
+def normalize_map(noise_map):
+    max_val = np.max(noise_map)
+    min_val = np.min(noise_map)
+    return (noise_map - min_val) / (max_val - min_val) * 255
+
+
+def create_image(noise_map, filename):
+    filename = f"./saves/{filename}.png"
+    image = Image.fromarray(noise_map)
+    image = image.convert('L')  # Convert to grayscale
+    image.save(filename)
--- window_handler.py
+++ window_handler.py
@@ -1,10 +1,11 @@
 import customtkinter
 import PIL
-
 from map_tooling import overworld_generator
 
 app_window_name = "World Weaver"
-app_window_resolution = "1366x768"
+app_window_width = 1920
+app_window_height = 1080
+app_window_resolution = f"{app_window_width}x{app_window_height}"
 logo_path = "assets/logo.png"
 icon_path = "assets/icon.ico"
 
@@ -12,6 +13,12 @@ icon_path = "assets/icon.ico"
 class WorldWeaverWindow(customtkinter.CTk):
     def __init__(self):
         super().__init__()
+        self.persistence_slider = None
+        self.octaves_slider = None
+        self.scale_slider = None
+        self.lacunarity_slider = None
+        self.map_preview_label = None
+        self.map_name_entry = None
         self.logo_label = None
         self.logo_photo_image = None
         self.logo_image = None
@@ -61,10 +68,75 @@ class WorldWeaverWindow(customtkinter.CTk):
     # NEW WORLD PAGE
     # -----------------------------------------------------------------------------------------------------------------
     def setup_new_world_page(self):
-        customtkinter.CTkButton(self.new_world_page, text="Back",
-                                command=self.show_home_page).pack(pady=10)
-        customtkinter.CTkButton(self.new_world_page, text="Generate noise map",
-                                command=overworld_generator.generate_overworld()).pack(pady=10)
+        # Split the frame into 2 panels
+        left_panel_screen_coverage = 0.2
+        left_panel_input_coverage = 0.95
+
+        left_panel_width = int(left_panel_screen_coverage * app_window_width)
+        left_panel_input_width = int(left_panel_width * left_panel_input_coverage)
+        back_button_padding = left_panel_width - left_panel_input_width
+
+        left_panel = customtkinter.CTkFrame(self.new_world_page, width=left_panel_width,
+                                            border_color="gray",
+                                            border_width=2)
+        left_panel.pack_propagate(False)
+        right_panel = customtkinter.CTkFrame(self.new_world_page,
+                                             border_color="gray",
+                                             border_width=2)
+
+        # Back Button
+        back_button = customtkinter.CTkButton(left_panel, text="Back", command=self.show_home_page,
+                                              width=left_panel_input_width)
+        back_button.pack(pady=back_button_padding)
+        # Map Name Field
+        self.map_name_entry = customtkinter.CTkEntry(left_panel, placeholder_text="Enter map name",
+                                                     width=left_panel_input_width)
+        self.map_name_entry.pack(pady=5)
+        # Sliders for noise map variables
+        # Scale Slider
+        self.scale_slider = customtkinter.CTkSlider(left_panel, from_=10, to=500, width=left_panel_input_width)
+        self.scale_slider.set(100)  # Default value
+        self.scale_slider.pack(pady=5)
+
+        # Octaves Slider
+        self.octaves_slider = customtkinter.CTkSlider(left_panel, from_=1, to=10, width=left_panel_input_width)
+        self.octaves_slider.set(6)  # Default value
+        self.octaves_slider.pack(pady=5)
+
+        # Persistence Slider
+        self.persistence_slider = customtkinter.CTkSlider(left_panel, from_=0, to=1, width=left_panel_input_width)
+        self.persistence_slider.set(0.5)  # Default value
+        self.persistence_slider.pack(pady=5)
+
+        # Lacunarity Slider
+        self.lacunarity_slider = customtkinter.CTkSlider(left_panel, from_=1, to=4, width=left_panel_input_width)
+        self.lacunarity_slider.set(2.0)  # Default value
+        self.lacunarity_slider.pack(pady=5)
+
+        # Button to preview the noise map
+        customtkinter.CTkButton(left_panel, text="Preview Noise Map", command=self.preview_noise_map,
+                                width=left_panel_input_width).pack(pady=5)
+
+        # Button to save the noise map
+        customtkinter.CTkButton(left_panel, text="Save Noise Map", command=self.save_noise_map,
+                                width=left_panel_input_width).pack(pady=5)
+
+        # Placeholder for map preview in the right panel
+        self.map_preview_label = customtkinter.CTkLabel(right_panel, text="Map Preview Here")
+        self.map_preview_label.pack(expand=True)
+        left_panel.pack(side='left', fill='y')
+        right_panel.pack(side='right', fill='both', expand=True)
+
+    def preview_noise_map(self):
+        scale_value = int(self.scale_slider.get())
+        octaves_value = self.octaves_slider.get()
+        persistence_value = self.persistence_slider.get()
+        lacunarity_value = self.lacunarity_slider.get()
+        file_name_to_use = self.map_name_entry.get()
+        overworld_generator.generate_overworld(file_name_to_use, scale_value, octaves_value, persistence_value, lacunarity_value)
+
+    def save_noise_map(self):
+        pass
 
     def show_new_world_page(self):
         self.home_page.pack_forget()
