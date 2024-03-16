4ddddb8bc9ee51f04d925d963c6e6018bc9f41f5
Nathan <nathanfenwick@rocketmail.com>
2024-03-16 04:03:42 +0000

    Tuning the land type values and added the new land type, snow.

--- map_tooling/over_world_generator.py
+++ map_tooling/over_world_generator.py
@@ -39,31 +39,32 @@ def save_coloured_map(image, save_name):
 
 
 def create_coloured_image(noise_map):
-    print("entering")
     height, width = noise_map.shape
     coloured_image = Image.new("RGB", (width, height))
     pixels = coloured_image.load()
 
     # Define height range thresholds
-    water_upper = 80
+    water_upper = 90
     sand_upper = 120
-    dirt_upper = 160
+    dirt_upper = 140
     forest_upper = 200
+    stone_upper = 220
     # Mountain/Stone is everything above forest_upper
     for y in range(height):
         for x in range(width):
             height_value = noise_map[y, x]
-            # Map height values to colours
             if height_value <= water_upper:  # Water
-                colour = (0, 0, 255)  # Blue
+                colour = (0, 0, 255)
             elif height_value <= sand_upper:  # Sand
-                colour = (194, 178, 128)  # Sandy colour
+                colour = (194, 178, 128)
             elif height_value <= dirt_upper:  # Dirt
-                colour = (139, 69, 19)  # Brown
+                colour = (139, 69, 19)
             elif height_value <= forest_upper:  # Forest
-                colour = (34, 139, 34)  # Forest green
-            else:  # Mountain/Stone
-                colour = (169, 169, 169)  # Gray
+                colour = (34, 139, 34)
+            elif height_value <= stone_upper:  # Stone
+                colour = (169, 169, 169)
+            else:  # Snow
+                colour = (255, 255, 255)
             pixels[x, y] = colour
     return coloured_image
 
